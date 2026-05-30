62d8db2ef67e5407116d47fd33ccecce53258fa2
David Brito <39559632+davbrito@users.noreply.github.com>
2026-05-30 14:38:55 +0000

    refactor

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,8 @@ import { dbMiddleware } from "./db/index.js";
 import { admin } from "./routes/admin.js";
 import { rates } from "./routes/rates.js";
 
-const app = new Hono()
+export const app = new Hono()
   .basePath("/api")
   .use(httpInstrumentationMiddleware({ serviceName: "tasero-api" }), dbMiddleware)
   .route("/admin", admin)
   .route("/rates", rates);
-
-export default app;

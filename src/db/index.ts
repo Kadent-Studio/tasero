0c3d6b3fea98362050d43ef28b47f066d7891ac7
David Brito <39559632+davbrito@users.noreply.github.com>
2026-05-30 15:28:55 +0000

    instrument drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,12 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { createMiddleware } from "hono/factory";
 import relations from "./relations.js";
+import { instrumentDrizzleClient } from "@kubiks/otel-drizzle";
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql, relations });
+
+instrumentDrizzleClient(db, { dbName: "tasero-db", tracerName: "tasero-api" });
 
 export type Db = typeof db;
 
